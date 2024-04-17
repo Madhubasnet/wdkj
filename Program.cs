925e99b6c39a17fb0a404bc5cd225a75daff6139
Madhubasnet <167259873+Madhubasnet@users.noreply.github.com>
2024-04-17 15:15:41 +1000

    change
    need to change

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿static void Main(string[] args)
+{
+    int gradecounter = 1;
+    int total = 0;
+    while (gradecounter <= 5)
+    {
+        Console.Write("Enter the grade: ");
+        int grade = Convert.ToInt32(Console.ReadLine());
+        total = total + grade;
+        gradecounter = gradecounter + 1;
+    }
+    Console.WriteLine("The average of the class is: " + total);
+}
