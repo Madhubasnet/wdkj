67f62de5125d9e8d6874c5a982178a14a6edd471
Madhubasnet <167259873+Madhubasnet@users.noreply.github.com>
2024-04-17 16:24:10 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,14 +2,14 @@
 {
     int gradecounter = 1;
     int total = 0;
-    while (gradecounter <= 5)
+    while (gradecounter <= 100)
     {
         Console.Write("Enter the grade: ");
         int grade = Convert.ToInt32(Console.ReadLine());
         
         
         total = total + grade;
-        gradecounter = gradecounter + 1;
+        gradecounter = gradecounter + 120;
     }
     Console.WriteLine("The average of the class is: " + total);
 }
